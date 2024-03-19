cbc2e3be0c90bf5d82098facaf149a799b45c5a1
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2024-03-19 01:05:19 +0100

    added methods

--- Linked list/constructor.py
+++ Linked list/constructor.py
@@ -28,8 +28,37 @@ class LinkedList:
         self.length += 1
         return True
     
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
 
 my_linked_list = LinkedList(15)
 my_linked_list.append(10)
 my_linked_list.append(5)
 my_linked_list.print_list()
+print(my_linked_list.pop().value)
+print(my_linked_list.pop().value)
+print(my_linked_list.pop().value)
+print(my_linked_list.pop())
